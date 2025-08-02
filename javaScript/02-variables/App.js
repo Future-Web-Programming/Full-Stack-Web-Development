eae8d691e147aa4990f3a10d34292c98afc50bcc
Waseem Malik <waseemprogrammerofficial@gmail.com>
2025-08-02 19:23:42 +0500

    js chapter-02 added

---
+++ javaScript/02-variables/App.js
@@ -0,0 +1,40 @@
+// declaring variable
+
+// let userName = "ghazian";
+// console.log(userName)
+// // const PI;
+// var price = 10; 
+
+// differnece between let , const and var 
+
+// let userName = 'ghasdfin';
+// userName = 'ghazi'
+// console.log(userName)
+// const price = 20;
+// price = 40;
+// console.log(price)
+
+// var price = 20;
+// price = 30;
+// var price = 40;
+// console.log(price)
+
+// let price = 20;
+// let price = 40;
+// console.log(price)
+
+// let userName = null; 
+// console.log(userName)
+
+// let username = 'green'; //global scope
+
+// function welcome(){
+//     let status = true;
+
+//     console.log(username)
+// }
+// {
+
+// }
+// console.log(status)
+// welcome()
