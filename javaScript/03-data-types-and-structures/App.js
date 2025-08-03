7d5a560db2d3010df3bcf62f1b99bec9af184247
Waseem Malik <waseemprogrammerofficial@gmail.com>
2025-08-03 09:23:33 +0500

    js chapter-03 added

---
+++ javaScript/03-data-types-and-structures/App.js
@@ -0,0 +1,99 @@
+// imutable or primitive data types 
+
+// string 
+
+// let userName = "jhon";
+// console.log(typeof userName)
+
+// number 
+
+// let rollNumber = 23;
+// console.log(typeof rollNumber)
+
+// decimal or integer
+
+// const PI = 3.14
+// console.log(typeof PI)
+
+// boolean 
+
+// let isloggedIn = true;
+// let hasAccess = false;
+
+// console.log(typeof isloggedIn)
+// console.log(isloggedIn)
+// console.log(typeof hasAccess)
+// console.log( hasAccess)
+
+// undefined 
+
+// let email;
+// console.log(email)
+// console.log(typeof email)
+
+// null 
+
+// let score = null;
+
+// console.log(score)
+// console.log(typeof score)
+
+// symbol 
+
+// const id = Symbol("userid");
+// console.log(id)
+// console.log(typeof id)
+
+// const id = Symbol(10);
+// console.log(id)
+// console.log(typeof id)
+
+// let bigNumber = 293024039403430493043434n;
+// console.log(bigNumber)
+// console.log(typeof bigNumber)
+
+// muteable / non primitive data types
+
+
+// object
+// let userOne = {
+//     name: 'john',
+//     className: 'it',
+//     rollNumber: 20
+// }
+
+// userOne.name = "ghazi";
+
+// console.log(userOne)
+// console.log(userOne.name)
+
+// array
+// let fruits = ["apple", "mango", 'santra'];
+
+// fruits[0] = "kela";
+
+// console.log(fruits)
+// console.log(fruits[0])
+
+// function add(){
+//     console.log('adding')
+// }
+
+// console.log(typeof add)
+
+// const date = new Date();
+
+// console.log(typeof date)
+
+// let price = 10;
+
+// console.log(typeof NaN)
+
+// let s1 = Symbol('id');
+// let s2 = Symbol('id');
+
+// console.log(s1 === s2);
+
+
+// console.log("5" + 1)
+// console.log(1 + "5")
