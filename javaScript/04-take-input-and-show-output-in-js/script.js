eab5846fb1cb6ffa215adcbc15a2da1e5805fbb4
Waseem Malik <waseemprogrammerofficial@gmail.com>
2025-08-04 15:21:32 +0500

    js chapter-04 and 05 added

---
+++ javaScript/04-take-input-and-show-output-in-js/script.js
@@ -0,0 +1,20 @@
+
+// taking user input
+// let city = prompt("what's your city Name?", "Karachi");
+
+// showing user data / output
+
+// console.log(city)
+// alert(city)
+// document.write(city)
+
+// problem
+// greeting app , which takes user input name then welcome to the user with any message
+
+let userName = prompt("Enter your Good Name?");
+let message = "Hallo " + userName + " Welcome to the world of Programming";
+
+
+console.log(message)
+document.write(message)
+alert(message)
---
+++ javaScript/05-operators/script.js
@@ -0,0 +1,185 @@
+// arithmatic operations
+
+// + addition
+
+// let num1 = 10;
+// let num2 = 20;
+// let sum = num1 + num2;
+// console.log("Answer is: " + sum)
+
+
+// - subtraction
+// let num1 = 10;
+// let num2 = 20;
+// let sub = num1 - num2;
+// console.log("Sub is: " , sub)
+
+// * multiplication
+// let num1 = 2;
+// let num2 = 4;
+// console.log("Multiplication answer is: ", num1 * num2);
+
+// / division
+// let num1 = 2;
+// let num2 = 4;
+// let div = num1 / num2;
+// console.log("Division is: ",div)
+
+// % remainder
+
+// let num1 = 10;
+// let num2 = 3;
+// let reminder = num1 % num2;
+// console.log(reminder)
+
+
+// ** power 
+
+// let num1 = 2;
+// let num2 = 4;
+// let power = num1 ** num2;
+// console.log("The answer is: ", power)
+
+// ++ increment
+
+// post increment
+// let num1 = 4;
+// let num2 = num1++;
+// console.log(num2)
+// console.log(num1)
+// pre increment
+// let x = 2;
+// let y = ++x;
+// console.log(x);
+// console.log(y);
+// -- decrement
+
+// post decrement
+
+// let x = 20;
+// let y = x--;
+// console.log(x)
+// console.log(y)
+
+// pre decrement
+// let num1 = 10;
+// let num2 = --num1;
+// console.log(num1)
+// console.log(num2)
+
+// assignment operators
+
+// = assignment operator
+
+// let userName = "jhon";
+
+
+// += addition assigment
+
+// let num1 = 10;
+// num1 += 5; 
+// num1 = num1 + 5;
+
+// console.log(num1)
+
+// -= subtraction assigmnet
+// let num1 = 10;
+// num1 = num1 - 2;
+// // num1 -= 2;
+// console.log(num1)
+// *= multiplication assigment
+// let num1 = 2;
+// num1 *= 2;
+// console.log(num1)
+// /= divsion assignment
+// let num = 20;
+
+// num /= 2;
+// num = num / 2;
+
+// console.log(num)
+
+// %= remainder assignment
+// let num = 10;
+// num = num % 3;
+// num %= 3;
+// console.log(num)
+
+// **= exponent assingment
+
+// let num = 2;
+
+// num **=2;
+// num = num ** 2;
+
+// console.log(num)
+
+// ===================== Comaprison Operators ==========
+
+// == Equal to Operator
+
+// let age = 10;
+
+// console.log(age == 10);
+
+// !=  Not Equal to 
+
+// let year = 2025;
+// console.log(year != 2022)
+
+// > greather then
+
+// let age = 20;
+// console.log(age > 20)
+
+// < less then
+// let age = 21;
+// console.log(age < 22)
+
+//  >= Greather then or equal to 
+
+// let age = 20;
+// console.log(age >= 21);
+
+//  <= Less then or equal to
+
+// let year = 2020;
+// console.log(year <= 2019)
+
+// === stricly equal to 
+
+// let rollNumber = "20";
+// console.log(typeof rollNumber)
+// console.log(rollNumber === 20)
+
+// !== stricly not equal to
+
+// let PI = "3.14";
+// console.log(PI !== 3.14)
+
+// =================== Logical Operators ============
+
+// let userLoggedIn = true;
+// let hasAccess = true;
+// console.log(userLoggedIn == true && hasAccess == true);
+
+// logical || OR
+
+// let age = 20;
+// let hasPermissionToVote = true;
+
+// console.log(age == 18 || hasPermissionToVote == false)
+
+
+// ! Logical Not 
+
+// let hasAccess = false;
+
+// console.log(!hasAccess)
+
+
+// string concatination operator
+
+// let string1 = "Hallo";
+// let string2 = "Welcome to the Future of Programming";
+// console.log(string1,string2)
