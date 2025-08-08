c15907e5a3278e70e5a0b80dbc508f3a6339764c
Waseem Malik <waseemprogrammerofficial@gmail.com>
2025-08-09 04:31:20 +0500

    js chapter-06 added

--- javaScript/06-control-flow-conditional-statements/script.js
+++ javaScript/06-control-flow-conditional-statements/script.js
@@ -1,36 +1,208 @@
-// arithmatic operations
+// license checker 
 
-// + 
-let num1 = Number(prompt("Enter number 1", 0));
-let num2 = Number(prompt("Enter number 2", 1));
-// type conversion 
+// let age = 17;
+// let state = 'pun'
+// if(age >= 18){
+//     console.log("yes you can drive")
+// }
 
-// console.log(typeof num1)
-// console.log(typeof num2)
-// let sum = num1 + num2;
-// console.log(sum)
+// nested if 
+
+// if(state == 'pun'){
+//     if(age >= 18){
+//         console.log("yes you can drive")
+//     }
+// }
+
+// if(state == 'pun' && age >= 18){
+//     console.log("Yes you can drive")
+// }
+// else
+//     console.log("sorry u are under age")
 
 
-// -
-// let sub = num1 - num2;
-// console.log(sub)
-// *
-// let multi = num1 * num2;
-// console.log(multi)
-// / 
-// let div = num1  / num2;
-// console.log(div)
-// %
-// let remainder = num1 % num2;
-// console.log(remainder)
-// **
-// let power = num1 ** num2;
-// console.log(power)
-// ++
-// post increment 
-// pre increment 
+// if else if 
 
-console.log(num1)
-console.log(num2)
-// --
-// num1--;
+// when you want to check multiplie conditions 
+
+// month checker based on the given number
+
+// let month = 13;
+
+// if(month === 1){
+//     console.log('Jan')
+// }else if(month === 2){
+//     console.log("Feb")
+// }else if(month === 3){
+//     console.log("Mar")
+// }else if(month === 4){
+//     console.log("Apr")
+// }else if(month === 5){
+//     console.log("May")
+// }else if(month === 6){
+//     console.log("June")
+// }else if(month === 7){
+//     console.log("Jul")
+// }else if(month === 8){
+//     console.log("aug")
+// }else if(month === 9){
+//     console.log("Sep")
+// }else if(month === 10){
+//     console.log("Oct")
+// }else if(month === 11){
+//     console.log("Nov")
+// }else if(month === 12){
+//     console.log("Dec")
+// }else {
+//     console.log("Invalid Month Number")
+// }
+
+// ternary operator 
+
+// let age = 16;
+// if(age >= 18){
+//     console.log("Yes u can drive")
+// }else{
+//     console.log("Sorry u can't drive")
+// }
+
+// let result = age >= 18 ? "You can drive" : "sorry You can't drive";
+
+// console.log(result)
+
+// let age = 20;
+// let state = 'pun'
+
+// let result = age > 18 ? (state == 'pun' ? "Yes you can drive" : "State isn't valid") : "Sorry you can't drive" 
+
+// console.log(result)
+
+// project grade calculator
+
+// let marks = 92;
+// if(marks >= 90){
+//     console.log('U scored Grade A+')
+// }else if(marks >= 80){
+//     console.log("U socred Grade A")
+// }else if(marks >= 70){
+//     console.log("U scored Grade B+")
+// }else if(marks >=60){
+//     console.log("U Scored Grade B")
+// }else if(marks >= 33){
+//     console.log("U Scored Grade C")
+// }else {
+//     console.log("Sorry You failed, better luck for next time")
+// }
+// switch statement 
+// let month = 2;
+// switch(month){
+//     case 1: 
+//         console.log("Jan")
+//         break;
+//     case 2:
+//         console.log("Feb")
+//         break;
+//     case 3:
+//         console.log("Mar")
+//         break;
+//     case 4:
+//         console.log("Apr")
+//         break;
+//     case 5:
+//         console.log("May")
+//         break;
+//     case 6:
+//         console.log("Jun")
+//         break;
+//     case 7:
+//         console.log("Jul")
+//         break;
+//     case 8:
+//         console.log("Aug")
+//         break;
+//     case 9:
+//         console.log("Sep")
+//         break;
+//     case 10:
+//         console.log("Oct")
+//         break;
+//     case 11:
+//         console.log("Nov")
+//         break;
+//     case 12:
+//         console.log("Dec")
+//         break;
+//     default:
+//         console.log("Invalid Month Number")
+//         break;
+// }
+
+// let marks = 32;
+// switch(true){
+//     case marks >= 90:
+//         console.log("Grade A+")
+//         break;
+//     case marks >= 80:
+//         console.log("Grade A")
+//         break;
+//     case marks >= 70:
+//         console.log("Grade B+")
+//         break;
+//     case marks >= 60:
+//         console.log("Grade B")
+//         break;
+//     case marks >= 33:
+//         console.log("Grade C")
+//         break;
+//     default:
+//         console.log("Sorry you failed, better luck for next time")
+//         break;  
+// }
+
+// Day message app
+
+
+// let day = new Date().getDay()
+
+// switch(day){
+//     case 0:
+//         console.log("Sunday: Relax")
+//         break;
+//     case 1:
+//         console.log("Monday: Hurry Working Day")
+//         break;
+//     case 2:
+//         console.log("Tuesday - Keep going")
+//         break;
+//     case 3:
+//         console.log("Wednesday: Being tired ")
+//         break;
+//     case 4: 
+//         console.log("Thursday - Keep going it's still thursday")
+//         break;
+//     case 5: 
+//         console.log("Friday - Blesed day")
+//         break;
+//     case 6:
+//         console.log("Saturday - enjoy the weekend")
+//         break;
+//     default:
+//         console.log("INvalid day")
+//         break;  
+
+// }
+
+// let marks = 0;
+// let updateMarks = marks || 12;
+// console.log(updateMarks)
+
+// login system
+
+// let userName = 'aasdf';
+// let password = 'admin12'
+
+// if(userName === 'admin' && password === 'admin12345'){
+//     console.log("welcome to dashboard");
+// }else{
+//     console.log("Sorry your credentionals are not valid")
+// }
